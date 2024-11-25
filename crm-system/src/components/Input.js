5480981de394c9df0ebd6ea48cffeee125ebfbc2
Поленов Максим <m.polenov@msk.i-teco.ru>
2024-11-25 16:24:31 +0300

    Initial insert and button

---
+++ crm-system/src/components/Input.js
@@ -0,0 +1,8 @@
+import './Input.css'
+export default function Input(){
+    return(
+        <>
+        <input placeholder = "Task To Be Done..."></input><button>Add</button>
+        </>
+    )
+}
